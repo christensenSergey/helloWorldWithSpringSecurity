8c59a4b1e204b0fe7f1765dc0eb155287fa65d01
Sergey Christensen <christensen.sergey@gmail.com>
2019-02-02 20:32:03 +0200

    Initial commit

---
+++ src/main/java/com/gmail/at/sergey/christensen/helloWorldWithSpringSecurity/HelloWorldWithSpringSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.gmail.at.sergey.christensen.helloWorldWithSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created 02.02.2019
+ * 
+ * @author Sergey Christensen
+ */
+
+@SpringBootApplication
+public class HelloWorldWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWithSpringSecurityApplication.class, args);
+	}
+
+}
